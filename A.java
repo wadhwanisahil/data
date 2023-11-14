3974362cbc6fb67f05c58d1f9185b04cdba2fee9
wadhwani sahil <136256288+wadhwanisahil@users.noreply.github.com>
2023-11-14 10:09:12 -0800

    Update A.java

--- A.java
+++ A.java
@@ -2,5 +2,7 @@ public class A {
     public static void main(String[] args) {
         System.out.println("sahil");
         System.out.println("mujeeb");
+          System.out.println("mujeeb");
+        
     }
 }
